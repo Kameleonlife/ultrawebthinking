5372443c2dacdba48b98ab3129d22b4e2f33cc20
Web8Kameleon <jonadeals@gmail.com>
2025-03-06 09:29:38 +0100

    Fshirë App.test.js sepse nuk përdoret

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('kontrollon nëse renderohet teksti', () => {
-  render(<App />);
-  const textElement = screen.getByText(/Ultrawebthinking është LIVE/i);
-  expect(textElement).toBeInTheDocument();
-});
-
